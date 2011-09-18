027dbf0fa5151561d285d934664375e5c0ea26f9
Brian Partridge <thelaughingman@gmail.com>
2011-09-18 19:55:14 -0400

    initial empty project

---
+++ BPKitTests/BPKitTests.h
@@ -0,0 +1,17 @@
+//
+//  BPKitTests.h
+//  BPKitTests
+//
+//  Created by Brian Partridge on 9/18/11.
+//  Copyright 2011 Brian Partridge. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface BPKitTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ BPKitTests/BPKitTests.m
@@ -0,0 +1,33 @@
+//
+//  BPKitTests.m
+//  BPKitTests
+//
+//  Created by Brian Partridge on 9/18/11.
+//  Copyright 2011 Brian Partridge. All rights reserved.
+//
+
+#import "BPKitTests.h"
+
+
+@implementation BPKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BPKitTests");
+}
+
+@end
