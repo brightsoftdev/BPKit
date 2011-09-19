fb3fbe159a056900d6ebc4794164dc3b1ccfe1c2
Brian Partridge <thelaughingman@gmail.com>
2011-09-18 20:48:58 -0400

    import categories in library header

---
+++ BPKit/BPKit.h
@@ -0,0 +1,13 @@
+//
+//  BPKit.h
+//  BPKit
+//
+//  Created by Brian Partridge on 9/18/11.
+//  Copyright 2011 Brian Partridge. All rights reserved.
+//
+
+#pragma mark - Categories
+
+#import "NSString+BPKit.h"
+#import "NSDictionary+BPKit.h"
+#import "NSURL+BPKit.h"
